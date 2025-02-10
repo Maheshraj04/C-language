6f7550d818174a4bc3f2d20c46a62cfa1fded99c
Maheshraj04 <maheshraj6761@gmail.com>
2025-02-10 13:49:04 +0530

    Find the Ap

---
+++ Ap.c
@@ -0,0 +1,13 @@
+//display ap 1,2,5,7,9,....n
+#include<stdio.h>
+int main(){
+    int n;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+for (int i = 1; i < 2*n-1; i=i+2)
+{
+    printf("%d ", i);
+}
+
+    return 0;
+}
